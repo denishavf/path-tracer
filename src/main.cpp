6869b58e88ef01bcd9e9fb32baa94778f3db6174
denishavf <denishavf@pm.me>
2023-05-11 04:11:06 +0300

    add random scene generator

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,54 @@
 #include <iostream>
 #include <memory>
 
+
+hittable_list<double> random_scene() {
+    hittable_list<double> world;
+
+    auto ground_material = 
+        make_shared<lambertian<double>>(color<double>(.5, .5, .5));  
+    world.add(make_shared<sphere<double>>(point3<double>(0, -1000, 0), 1000, ground_material));
+
+    for (int a = -11; a < 11; ++a) {
+        for (int b= -11; b < 11; ++b) {
+            auto choose_mat = random_double();
+            point3<double> center(
+                    a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
+
+            if ((center - point3<double>(4, 0.2, 0)).len() > 0.9) {
+                shared_ptr<material<double>> sphere_material;
+
+                if (choose_mat < 0.8) {
+                    // diffuse
+                    auto albedo = color<double>::random() * color<double>::random();
+                    sphere_material = make_shared<lambertian<double>>(albedo);
+                    world.add(make_shared<sphere<double>>(
+                                center, 0.2, sphere_material));
+                }
+                else if (choose_mat < 0.95) {
+                    //metal
+                    auto albedo = color<double>::random(.5, 1);
+                    auto fuzz = random_double(.5, 1);
+                    sphere_material = make_shared<metal<double>>(albedo, fuzz);
+                    world.add(make_shared<sphere<double>>(
+                                center, .2, sphere_material));
+                }
+                else {
+                    // glass
+                    sphere_material = make_shared<dielectric<double>>(0.5);
+                    world.add(make_shared<sphere<double>>(
+                                center, 0.2, sphere_material));
+                }
+            }
+        }
+    }
+
+    auto material1 = make_shared<dielectric<double>>(1.5);
+    world.add(make_shared<sphere<double>>(point3<double>(0, 1, 0), 1.0, material1));
+
+    return world;
+}
+
 color<double> ray_color(
         const ray<double>& r, 
         const hittable<double>& world,
@@ -40,43 +88,21 @@ color<double> ray_color(
 }
 
 int main() {
-    const auto aspect_ratio = 16.0 / 9.0;
-    const int image_width = 400;
+    const auto aspect_ratio = 3.0 / 2.0;
+    const int image_width = 1200;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int samples_per_pixel = 100;
+    const int samples_per_pixel = 50;
     const int max_depth = 50;
 
     // world
-    auto R = cos(pi / 4);
-    hittable_list<double> world;
-
-    
-    auto material_ground = 
-        make_shared<lambertian<double>>(color<double>(.8, .8, .0));
-    auto material_center = 
-        make_shared<lambertian<double>>(color<double>(0.1, 0.2, 0.5));
-    auto material_left = 
-        make_shared<dielectric<double>>(1.5);
-    auto material_right = 
-        make_shared<metal<double>>(color<double>(.8, .6, .2), 0.0);
-
-    world.add(make_shared<sphere<double>>(
-                point3<double>(.0, -100.5, -1.0), 100.0, material_ground));
-    world.add(make_shared<sphere<double>>(
-                point3<double>(.0, .0, -1.0), .5, material_center));
-    world.add(make_shared<sphere<double>>(
-                point3<double>(-1.0, .0, -1.0), .5, material_left));
-    world.add(make_shared<sphere<double>>(
-                point3<double>(-1.0, .0, -1.0), -0.4, material_left));
-    world.add(make_shared<sphere<double>>(
-                point3<double>(1.0, .0, -1.0), .5, material_right));
+    auto world = random_scene();
 
     // camera
-    point3<double> lookfrom(3, 3, 2); 
-    point3<double> lookat(0, 0, -1);
+    point3<double> lookfrom(13, 2, 3); 
+    point3<double> lookat(0, 0, 0);
     vec3<double> vup(0, 1, 0);
-    auto dist_to_focus = (lookfrom - lookat).len();
-    auto aperture = 2.0; 
+    auto dist_to_focus = 10.0;
+    auto aperture = 0.1; 
 
     camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
 
